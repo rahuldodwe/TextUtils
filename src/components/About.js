d88da90adab22c99577b3128483d4e4490135ce1
Rahul Dodwe <rahuldodwe7747909605@gmail.com>
2024-02-27 19:52:15 +0530

    removing bugs

--- src/components/About.js
+++ src/components/About.js
@@ -36,7 +36,7 @@ export default function About() {
     <div className="accordion-item">
       <h2 className="accordion-header">
         <button className="accordion-button" style={myStyle} type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-          Accordion Item #1
+          Analyze Your text
         </button>
       </h2>
       <div id="collapseOne" className="accordion-collapse collapse show" data-bs-parent="#accordionExample">
@@ -48,7 +48,7 @@ export default function About() {
     <div className="accordion-item">
       <h2 className="accordion-header">
         <button className="accordion-button collapsed" style={myStyle} type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-          Accordion Item #2
+          Free to use
         </button>
       </h2>
       <div id="collapseTwo" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
@@ -60,7 +60,7 @@ export default function About() {
     <div className="accordion-item">
       <h2 className="accordion-header">
         <button className="accordion-button collapsed" style={myStyle} type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-          Accordion Item #3
+          Browser Compatible
         </button>
       </h2>
       <div id="collapseThree" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
@@ -70,10 +70,7 @@ export default function About() {
       </div>
     </div>
         </div>
-        <div className="Container my-3">
-        <button onClick={toggleStyle} type="button" className="btn btn-primary">{btntext}</button>
-        </div>
-      
+       
     </div>
   )
 }
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -53,18 +53,18 @@ export default function TextForm(props) {
           <div className="mb-3">
             <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode==='dark'?'grey':'white', color: props.mode==='dark'?'white':'#042743'}} id="myBox" rows="8"></textarea>  
           </div>    
-          <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to Uppercse</button>
-          <button className="btn btn-primary mx-2" onClick={handleLoClick}>Convert to Lowercase</button>
-          <button className="btn btn-primary mx-2" onClick={handleClearClick}>Clear Text</button>
-          <button className="btn btn-primary mx-2" onClick={handleCopy}>Copy Text</button>
-          <button className="btn btn-primary mx-2" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
+          <button className="btn btn-primary mx-1 my-1" onClick={handleUpClick}>Convert to Uppercse</button>
+          <button className="btn btn-primary mx-1 my-1" onClick={handleLoClick}>Convert to Lowercase</button>
+          <button className="btn btn-primary mx-1 my-1" onClick={handleClearClick}>Clear Text</button>
+          <button className="btn btn-primary mx-1 my-1" onClick={handleCopy}>Copy Text</button>
+          <button className="btn btn-primary mx-1 my-1" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
 
 
 
     </div>
     <div className="container my-3" style={{color: props.mode==='dark'?'white':'#042743'}}>
         <h1>Your text summury</h1>
-        <p>{text.split(" ").length} words and {text.length} characters</p>
+        <p>{text.split(" ").filter((element)=>{return element.length!==0}).length} words and {text.length} characters</p>
         <p>{0.008 * text.split(" ").length} Minutes read</p>
         <h2>Preview</h2>
         <p>{text.length>0?text:"Enter something in the textbox above to previe it here"}</p>
