5aa06e140e1a2ef41c82acdd8ccb6fa271c7df48
Rahul Dodwe <rahuldodwe7747909605@gmail.com>
2024-03-01 19:38:44 +0530

    TextUtils update1

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -30,8 +30,8 @@ export default function TextForm(props) {
 
   const handleCopy = ()=> {
     // console.log("I am copy");
-    var text = document.getElementById("myBox");
-    navigator.clipboard.writeText(text.value);
+    // var text = document.getElementById("myBox");
+    navigator.clipboard.writeText(text);
     props.showAlert("Copied to Clipboard!", "success");
   }
 
@@ -63,7 +63,7 @@ export default function TextForm(props) {
     </div>
     <div className="container my-3" style={{color: props.mode==='dark'?'white':'#042743'}}>
         <h1>Your text summury</h1>
-        <p>{text.split(" ").filter((element)=>{return element.length!==0}).length} words and {text.length} characters</p>
+        <p>{text.split(/\s+/).filter((element)=>{return element.length!==0}).length} words and {text.length} characters</p>
         <p>{0.008 * text.split(" ").filter((element)=>{return element.length!==0}).length} Minutes read</p>
         <h2>Preview</h2>
         <p>{text.length>0?text:"Nothing to preview"}</p>
